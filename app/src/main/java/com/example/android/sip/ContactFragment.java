e7bb942677890db8b62738ab345245bc9630a8b5
irfanm96 <e15138@ce.pdn.ac.lk>
2019-05-16 17:20:36 +0530

    added filter to recylcr view

--- app/src/main/java/com/example/android/sip/ContactFragment.java
+++ app/src/main/java/com/example/android/sip/ContactFragment.java
@@ -6,9 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +19,7 @@ import java.util.List;
 public class ContactFragment extends Fragment {
 
     private View view;
+    private EditText etSearch;
 
     private RecyclerView recyclerView;
     private ArrayList<Contact> contactList;
@@ -27,13 +31,33 @@ public class ContactFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view=inflater.inflate(R.layout.contact_fragment,container,false);
+        view = inflater.inflate(R.layout.contact_fragment, container, false);
 
-        recyclerView=(RecyclerView) view.findViewById(R.id.rv_contact);
-        RecyclerViewAdapter recyclerViewAdapter=new RecyclerViewAdapter(getContext(),contactList);
+        etSearch = (EditText) view.findViewById(R.id.etSearch);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.rv_contact);
+        final RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(getContext(), contactList);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(recyclerViewAdapter);
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                recyclerViewAdapter.getFilter().filter(s);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         return view;
     }
 
@@ -41,10 +65,10 @@ public class ContactFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        contactList=new ArrayList<>();
-        contactList.add(new Contact("demo","200@ping.com","200"));
-        contactList.add(new Contact("Irfan","3000@ping.com","3000"));
-        contactList.add(new Contact("Wishma","3001@ping.com","3001"));
-        contactList.add(new Contact("Rishi","3002@ping.com","3002"));
+        contactList = new ArrayList<>();
+        contactList.add(new Contact("demo", "200@ping.com", "200"));
+        contactList.add(new Contact("Irfan", "3000@ping.com", "3000"));
+        contactList.add(new Contact("Wishma", "3001@ping.com", "3001"));
+        contactList.add(new Contact("Rishi", "3002@ping.com", "3002"));
     }
 }
