0da8e5c237bbb0e31798f79bf414bef4a980f682
Wishma L Herath <wisheslakshan@gmail.com>
2019-05-19 17:53:19 +0530

    outgonig and incoming layouts are added

--- app/src/main/java/com/example/android/sip/CallFragment.java
+++ app/src/main/java/com/example/android/sip/CallFragment.java
@@ -1,6 +1,9 @@
 package com.example.android.sip;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -21,6 +24,9 @@ public class CallFragment extends Fragment {
     ImageButton imageButton;
     EditText editText;
     private static final String TAG = "APP_DEBUG";
+    private ImageButton callButton;
+    private Dialog dialog;
+
     public CallFragment() {
     }
 
@@ -30,15 +36,35 @@ public class CallFragment extends Fragment {
 
         view=inflater.inflate(R.layout.call_fragment,container,false);
         imageButton=(ImageButton)view.findViewById(R.id.imgbtnNewContact);
-       editText=(EditText) view.findViewById(R.id.etPhoneNumber);
+        callButton=(ImageButton)view.findViewById(R.id.imgbtnNewCall);
+        editText=(EditText) view.findViewById(R.id.etPhoneNumber);
 
 
+        //callButton.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View v) {
+//                dialog = new Dialog(v.getContext());
+//                dialog.setContentView(R.layout.outgoing_call); //add here to get updated
+//               // dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//                dialog.show();
+
+            //}
+       // });
+        //edited
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), OutgoingCall.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+        //edited
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), ContactDetails.class);
                 intent.putExtra("STATUS", "NEW");
-                intent.putExtra("PHONE",editText.getText().toString());
+                intent.putExtra("PHONE", editText.getText().toString());
                 v.getContext().startActivity(intent);
             }
         });
--- app/src/main/java/com/example/android/sip/OutgoingCall.java
+++ app/src/main/java/com/example/android/sip/OutgoingCall.java
@@ -1,4 +1,50 @@
 package com.example.android.sip;
 
-public class OutgoingCall {
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class OutgoingCall extends Fragment {
+
+    View view;
+    private Button endButton;
+    private Dialog dialog;
+
+    public OutgoingCall() {
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        view=inflater.inflate(R.layout.outgoing_call,container,false);
+        endButton=(Button)view.findViewById(R.id.end_button);
+
+
+        endButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog = new Dialog(v.getContext());
+                dialog.setContentView(R.layout.call_ended);
+                dialog.show();
+            }
+        });
+
+//        endButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(), CallEnded.class);
+//                v.getContext().startActivity(intent);
+//            }
+//        });
+        return view;
+    }
 }
+
