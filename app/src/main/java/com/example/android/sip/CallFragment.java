86ab70e9608b152ae51d6848c1f022c79d3e8da9
irfanm96 <e15138@ce.pdn.ac.lk>
2019-05-19 21:53:38 +0530

    fixed issues in new outgoing alert

--- app/src/main/java/com/example/android/sip/CallFragment.java
+++ app/src/main/java/com/example/android/sip/CallFragment.java
@@ -1,23 +1,18 @@
 package com.example.android.sip;
 
-<<<<<<< HEAD
 import android.app.Dialog;
-=======
 import android.content.Context;
->>>>>>> a01a5933302fa8b784a721a6a3b3d29dd60fb069
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -29,11 +24,10 @@ public class CallFragment extends Fragment {
     ImageButton imageButton;
     EditText editText;
     private static final String TAG = "APP_DEBUG";
-<<<<<<< HEAD
     private ImageButton callButton;
     private Dialog dialog;
-=======
->>>>>>> a01a5933302fa8b784a721a6a3b3d29dd60fb069
+    private Button hangUp;
+    Dialog mydialog;
 
     public CallFragment() {
     }
@@ -42,38 +36,36 @@ public class CallFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-<<<<<<< HEAD
-        view=inflater.inflate(R.layout.call_fragment,container,false);
-        imageButton=(ImageButton)view.findViewById(R.id.imgbtnNewContact);
-        callButton=(ImageButton)view.findViewById(R.id.imgbtnNewCall);
-        editText=(EditText) view.findViewById(R.id.etPhoneNumber);
-
-
-        //callButton.setOnClickListener(new View.OnClickListener() {
-            //@Override
-            //public void onClick(View v) {
-//                dialog = new Dialog(v.getContext());
-//                dialog.setContentView(R.layout.outgoing_call); //add here to get updated
-//               // dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-//                dialog.show();
-=======
         view = inflater.inflate(R.layout.call_fragment, container, false);
         imageButton = (ImageButton) view.findViewById(R.id.imgbtnNewContact);
+        callButton = (ImageButton) view.findViewById(R.id.imgbtnNewCall);
         editText = (EditText) view.findViewById(R.id.etPhoneNumber);
 
-        Log.d(TAG, "onCreateView: ");
->>>>>>> a01a5933302fa8b784a721a6a3b3d29dd60fb069
-
-            //}
-       // });
-        //edited
         callButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), OutgoingCall.class);
-                v.getContext().startActivity(intent);
+                mydialog = new Dialog(v.getContext(), android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+                mydialog.setContentView(R.layout.outgoing_call);
+                mydialog.show();
+                hangUp = (Button) mydialog.findViewById(R.id.btnHangUp);
+
+                hangUp.setOnClickListener(new View.OnClickListener() {
+                    private static final String TAG = "APP_DEBUG";
+
+                    @Override
+                    public void onClick(View v) {
+                        Log.d(TAG, "onClick: hang up clikced");
+                        mydialog.dismiss();
+
+                    }
+                });
+
+
             }
         });
+
+
         //edited
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,7 +84,7 @@ public class CallFragment extends Fragment {
         super.setUserVisibleHint(isVisibleToUser);
 
 
-        if (getActivity() != null && getView()!=null) {
+        if (getActivity() != null && getView() != null) {
             if (!isVisibleToUser) {
                 final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
 
