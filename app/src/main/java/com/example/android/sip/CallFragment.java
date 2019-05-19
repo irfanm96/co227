08af612ad7d173443fcac44cd46d22b8286f0d6f
Wishma L Herath <wisheslakshan@gmail.com>
2019-05-19 18:59:18 +0530

    Errors are solved now,but there is an error in activity_contact_details.xml

--- app/src/main/java/com/example/android/sip/CallFragment.java
+++ app/src/main/java/com/example/android/sip/CallFragment.java
@@ -1,10 +1,8 @@
 package com.example.android.sip;
 
-<<<<<<< HEAD
+
 import android.app.Dialog;
-=======
 import android.content.Context;
->>>>>>> a01a5933302fa8b784a721a6a3b3d29dd60fb069
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -29,11 +27,11 @@ public class CallFragment extends Fragment {
     ImageButton imageButton;
     EditText editText;
     private static final String TAG = "APP_DEBUG";
-<<<<<<< HEAD
+
     private ImageButton callButton;
     private Dialog dialog;
-=======
->>>>>>> a01a5933302fa8b784a721a6a3b3d29dd60fb069
+
+
 
     public CallFragment() {
     }
@@ -42,38 +40,40 @@ public class CallFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-<<<<<<< HEAD
+
         view=inflater.inflate(R.layout.call_fragment,container,false);
         imageButton=(ImageButton)view.findViewById(R.id.imgbtnNewContact);
         callButton=(ImageButton)view.findViewById(R.id.imgbtnNewCall);
         editText=(EditText) view.findViewById(R.id.etPhoneNumber);
 
 
-        //callButton.setOnClickListener(new View.OnClickListener() {
-            //@Override
-            //public void onClick(View v) {
-//                dialog = new Dialog(v.getContext());
-//                dialog.setContentView(R.layout.outgoing_call); //add here to get updated
-//               // dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-//                dialog.show();
-=======
-        view = inflater.inflate(R.layout.call_fragment, container, false);
-        imageButton = (ImageButton) view.findViewById(R.id.imgbtnNewContact);
-        editText = (EditText) view.findViewById(R.id.etPhoneNumber);
-
-        Log.d(TAG, "onCreateView: ");
->>>>>>> a01a5933302fa8b784a721a6a3b3d29dd60fb069
-
-            //}
-       // });
-        //edited
         callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), OutgoingCall.class);
-                v.getContext().startActivity(intent);
+                dialog = new Dialog(v.getContext());
+                dialog.setContentView(R.layout.outgoing_call); //add here to get updated
+                dialog.show();
+
+                //"callended xml should be poped up when press the end_button in outgoing call"
+
+
+//        view = inflater.inflate(R.layout.call_fragment, container, false);
+//        imageButton = (ImageButton) view.findViewById(R.id.imgbtnNewContact);
+//        editText = (EditText) view.findViewById(R.id.etPhoneNumber);
+//
+//        Log.d(TAG, "onCreateView: ");
+
+
             }
         });
+        //edited
+//        callButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(), OutgoingCall.class);
+//                v.getContext().startActivity(intent);
+//            }
+//        });
         //edited
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
